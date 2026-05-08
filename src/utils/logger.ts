e7b4ec9f3219c144c511dda4817f901763894cc4
rupalisolanki <rupalisolanki7876@gmail.com>
2026-05-08 20:54:19 +0530

    fix: add console transport in production for Render logs

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -34,11 +34,9 @@ export const logger = winston.createLogger({
   ],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new winston.transports.Console({
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.simple()
-    ),
-  }));
-}
+logger.add(new winston.transports.Console({
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+}));
